9cb46da1de98a2c76dd1f8011e5f3c473c331e83
PoloNord <matteo.bottazzi@outlook.com>
2026-03-15 01:04:42 +0100

    Fixed some issues 2

--- import_API.py
+++ import_API.py
@@ -2,9 +2,11 @@ import os
 import pandas as pd
 from entsoe import EntsoePandasClient
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
 
 def get_ENTSOe_KEY():
-    key = os.environ['ENTSOe_KEY']
+    load_dotenv()  # loads variables from .env into environment
+    key = os.getenv("ENTSOe_KEY")
     return key
 
 # Function to query installed capacity
@@ -25,19 +27,13 @@ def query_load_data(api_key, country_code, start_date, end_date):
     load_data = client.query_load(country_code, start=start_date, end=end_date)
     return load_data
 
-# Function to save data to CSV
-def save_to_csv(data, filename, start_date, end_date):
-    date_range = pd.date_range(start=start_date, end=(end_date - pd.Timedelta(hours=1)), freq='h')
-    data.insert(0, 'Date', date_range)
-    data.to_csv(filename, index=False)
-
 
 # Main function to orchestrate the process
 def main():
     api_key = get_ENTSOe_KEY()
     country_code = 'IT'
-    start_date = pd.Timestamp('2023-01-01', tz='Europe/Rome')
-    end_date = pd.Timestamp('2023-01-08', tz='Europe/Rome')
+    start_date = pd.Timestamp('2025-01-01', tz='Europe/Rome')
+    end_date = pd.Timestamp('2025-01-08', tz='Europe/Rome')
 
     print("installed capacity")
     installed_capacity = query_installed_capacity(api_key, country_code, start_date, end_date)
@@ -77,7 +73,7 @@ def main():
     print("generation data")
     # Query generation data for each type of PSR
     generation_data = {}
-    psr_types = ['B01', 'B09', 'B11', 'B16', 'B18', 'B19']
+    psr_types = ['B01', 'B09', 'B10', 'B11', 'B12', 'B13', 'B16', 'B18', 'B19']
     client = EntsoePandasClient(api_key=api_key)
     for psr_type in psr_types:
         # print(psr_type)
@@ -87,13 +83,15 @@ def main():
     
     # Create a new column for each of the psr types and add the data to it
     for psr_type in psr_types:
+        if isinstance(generation_data[psr_type], tuple):
+            generation_data[psr_type] = generation_data[psr_type].xs('Actual Aggregated', axis=1, level=1, drop_level=True)
         load_data[psr_type] = generation_data[psr_type]
 
     # Rename the columns in the DataFrame
     load_data = load_data.rename(columns=column_mapping)
 
 
-    save_to_csv(load_data, 'data/italy_load_generation.csv', start_date, end_date)
+    load_data.to_csv('data/italy_load_generation.csv', index=True)
 
 if __name__ == "__main__":
     main()
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from renewable-energy-realtime!")
-
-
-if __name__ == "__main__":
-    main()
